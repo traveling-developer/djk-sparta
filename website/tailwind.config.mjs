9c8152212bc04f64fc065e3b97507fbfae1c791e
traveling-developer <github@menzel-it.de>
2024-10-19 14:57:03 +0200

    Setup tailwind config with colors and basic light theme

--- website/tailwind.config.mjs
+++ website/tailwind.config.mjs
@@ -3,6 +3,19 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {},
+		colors: {
+			'darkGrey': '#242424',
+			'red': '#e02b20'
+		}
+	},
+	daisyui: {
+		themes: [
+			{
+				light: {
+					...require("daisyui/src/theming/themes")["light"],
+				},
+			},
+		],
 	},
 	plugins: [require('daisyui'),],
 }

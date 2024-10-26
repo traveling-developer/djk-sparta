b115c86b9498a8a17981fe7d994fcd712aee06a8
traveling-developer <github@menzel-it.de>
2024-10-26 20:52:46 +0200

    Implement the table tennis page with the display of the teams

---
+++ website/src/lib/table-tennis.ts
@@ -0,0 +1,46 @@
+import axios from 'axios';
+import * as cheerio from 'cheerio';
+
+interface Team {
+    name: string;
+    league: string;
+    rank: string;
+    points: string;
+    link: string;
+}
+
+const url = 'https://www.mytischtennis.de/clicktt/ByTTV/24-25/verein/207077/DJK-Sparta-Noris-Nuernberg/mannschaften/';
+
+export async function getTeams(): Promise<Team[]> {
+    try {
+        const { data } = await axios.get(url);
+
+        const $ = cheerio.load(data);
+
+        const teams: Team[] = [];
+
+        $('.table tbody tr').each((index, element) => {
+            let name = $(element).find('td').eq(0).text().trim().replace(/\s?\(.*?\)/, '');
+            if (name == 'Herren') {
+                name = 'Herren I';
+            }
+            const leagueElement = $(element).find('td').eq(1);
+            const league = leagueElement.text().trim().replace(/\s?\(Bayerischer TTV\)/, '').replace(/\s?\(Bayerischer TTV - Mittelfranken-Nord\)/, '');
+            const link = 'https://www.mytischtennis.de/' + leagueElement.children().first().attr('href')!;
+            const rank = $(element).find('td').eq(3).text().trim();
+            const points = $(element).find('td').eq(4).text().trim();
+
+            if (league.includes('Pokal')) {
+                return;
+            }
+
+            teams.push({ name: name, league, rank, points, link });
+        });
+
+        return teams;
+    } catch (error) {
+        console.error('Error downloading teams:', error);
+        return [];
+    }
+}
+

964e734a28e124c0d3bb5eda7ecf5ca2dd74fe8e
traveling-developer <github@menzel-it.de>
2025-03-12 19:34:55 +0100

    Add staging deployment to cloudflare pages

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -3,8 +3,10 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
 
+const siteUrl = process.env.SITE_URL
+
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://djkspartanoris.de',
+  site: 'https://' + siteUrl,
   integrations: [tailwind()]
 });

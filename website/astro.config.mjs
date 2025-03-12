06f9d607d69fb9ebe93752434ff34957fa153f1f
traveling-developer <github@menzel-it.de>
2025-03-12 18:39:48 +0100

    Update the config and the CNAME to point to djkspartanoris.de

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://sparta.debaggu.de',
+  site: 'https://djkspartanoris.de',
   integrations: [tailwind()]
 });

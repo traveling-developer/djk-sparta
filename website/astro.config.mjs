ff6b39f8d635ba321e028a674b70da0e0d331ac7
traveling-developer <github@menzel-it.de>
2025-04-19 16:01:39 +0200

    Add robots.txt for prod and dev and add sitemap to the page

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -3,10 +3,12 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
 
+import sitemap from '@astrojs/sitemap';
+
 const siteUrl = process.env.SITE_URL
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://' + siteUrl,
-  integrations: [tailwind()]
+  integrations: [tailwind(), sitemap()]
 });

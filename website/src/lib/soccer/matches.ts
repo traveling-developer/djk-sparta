a42e0974653bb8969ef61c1254076b1cee1b588c
traveling-developer <github@menzel-it.de>
2025-09-13 18:28:23 +0200

    Add adult soccer matches for season 25/26

--- website/src/lib/soccer/matches.ts
+++ website/src/lib/soccer/matches.ts
@@ -8,70 +8,277 @@ interface Match {
 export function getMatches(): Match[] {
   const matches = [
     {
-      date: "Fr. 21.03.25",
+      date: "Do. 26.09.25",
       time: "19:00",
-      homeTeam: "DJK Eibach",
-      guestTeam: "Sparta AH",
+      homeTeam: "SC Germania Nbg. III",
+      guestTeam: "DJK Sparta Noris",
+    },
+
+    {
+      date: "Sa. 14.09.25",
+      time: "13:00",
+      homeTeam: "Tuspo Heroldsberg II",
+      guestTeam: "DJK Sparta Noris",
     },
     {
-      date: "Fr. 28.03.25",
+      date: "Sa. 21.09.25",
+      time: "12:00",
+      homeTeam: "DJK Sparta Noris",
+      guestTeam: "SV Laufamholz II",
+    },
+
+    {
+      date: "Do. 03.10.25",
+      time: "12:00",
+      homeTeam: "DJK Sparta Noris",
+      guestTeam: "ASN Pfeil-Phönix II (B9)",
+    },
+    {
+      date: "Sa. 05.10.25",
+      time: "13:00",
+      homeTeam: "TV Glaishammer II (B9)",
+      guestTeam: "DJK Sparta Noris",
+    },
+    {
+      date: "Sa. 12.10.25",
+      time: "13:00",
+      homeTeam: "ASV Buchenbühl II (B9)",
+      guestTeam: "DJK Sparta Noris",
+    },
+    {
+      date: "Sa. 19.10.25",
+      time: "12:00",
+      homeTeam: "DJK Sparta Noris",
+      guestTeam: "SpVgg Mögeldorf 2000 IV",
+    },
+    {
+      date: "Sa. 26.10.25",
+      time: "12:00",
+      homeTeam: "DJK Sparta Noris",
+      guestTeam: "SC Germania Nbg. III",
+    },
+    {
+      date: "Sa. 02.11.25",
+      time: "13:00",
+      homeTeam: "FC Bayern Kickers U23 III",
+      guestTeam: "DJK Sparta Noris",
+    },
+    {
+      date: "Sa. 09.11.25",
+      time: "12:00",
+      homeTeam: "DJK Sparta Noris",
+      guestTeam: "DJK BFC Nürnberg II",
+    },
+    {
+      date: "Sa. 16.11.25",
+      time: "13:00",
+      homeTeam: "DJK Sparta Noris",
+      guestTeam: "TSV Maccabi Nürnberg II",
+    },
+    {
+      date: "Sa. 15.03.26",
+      time: "12:00",
+      homeTeam: "DJK Sparta Noris",
+      guestTeam: "SV Wacker Nürnberg II",
+    },
+    {
+      date: "Sa. 22.03.26",
+      time: "12:00",
+      homeTeam: "SpVgg Zabo Eintracht II",
+      guestTeam: "DJK Sparta Noris",
+    },
+    {
+      date: "Sa. 29.03.26",
+      time: "12:00",
+      homeTeam: "DJK Sparta Noris",
+      guestTeam: "Tuspo Heroldsberg II",
+    },
+    {
+      date: "Sa. 12.04.26",
+      time: "13:00",
+      homeTeam: "SV Laufamholz II",
+      guestTeam: "DJK Sparta Noris",
+    },
+    {
+      date: "Sa. 26.04.26",
+      time: "12:45",
+      homeTeam: "ASN Pfeil-Phönix II (B9)",
+      guestTeam: "DJK Sparta Noris",
+    },
+    {
+      date: "Sa. 03.05.26",
+      time: "12:00",
+      homeTeam: "DJK Sparta Noris",
+      guestTeam: "TV Glaishammer II (B9)",
+    },
+    {
+      date: "Sa. 10.05.26",
+      time: "12:00",
+      homeTeam: "DJK Sparta Noris",
+      guestTeam: "ASV Buchenbühl II (B9)",
+    },
+    {
+      date: "Sa. 17.05.26",
+      time: "15:00",
+      homeTeam: "SpVgg Mögeldorf 2000 IV",
+      guestTeam: "DJK Sparta Noris",
+    },
+    {
+      date: "Sa. 28.09.25",
+      time: "09:00",
+      homeTeam: "DJK Sparta Noris Bonifaz",
+      guestTeam: "ASV Fürth IV Armenia",
+    },
+    {
+      date: "Do. 03.10.25",
+      time: "09:30",
+      homeTeam: "FC Blau-Schwarz 88",
+      guestTeam: "DJK Sparta Noris Bonifaz",
+    },
+    {
+      date: "Sa. 05.10.25",
+      time: "09:00",
+      homeTeam: "DJK Sparta Noris Bonifaz",
+      guestTeam: "SC Germania Nbg. IIa (9er)",
+    },
+    {
+      date: "Sa. 12.10.25",
+      time: "09:30",
+      homeTeam: "ASV Zirndorf PM Wildner (9er)",
+      guestTeam: "DJK Sparta Noris Bonifaz",
+    },
+    {
+      date: "Sa. 19.10.25",
+      time: "09:00",
+      homeTeam: "DJK Sparta Noris Bonifaz",
+      guestTeam: "Zabo United",
+    },
+    {
+      date: "Sa. 26.10.25",
+      time: "10:00",
+      homeTeam: "Pegnitzkicker Nbg. Stadtverwaltung (9er)",
+      guestTeam: "DJK Sparta Noris Bonifaz",
+    },
+    {
+      date: "Sa. 02.11.25",
+      time: "09:00",
+      homeTeam: "DJK Sparta Noris Bonifaz",
+      guestTeam: "ATV 1873 Frankonia Nbg. Dritte",
+    },
+    {
+      date: "Do. 07.11.25",
       time: "19:00",
-      homeTeam: "Sparta AH",
-      guestTeam: "SC Worzeldorf",
+      homeTeam: "Kriemhild-Funker (9er)",
+      guestTeam: "DJK Sparta Noris Bonifaz",
     },
     {
-      date: "Sa. 05.04.25",
-      time: "16:00",
-      homeTeam: "SV Wacker",
-      guestTeam: "Sparta AH",
+      date: "Sa. 16.11.25",
+      time: "09:00",
+      homeTeam: "DJK Sparta Noris Bonifaz",
+      guestTeam: "FSV Stadeln 1b Westphal",
     },
     {
-      date: "Mi. 07.05.25",
+      date: "Sa. 23.11.25",
+      time: "10:00",
+      homeTeam: "FC Stein II (9er)",
+      guestTeam: "DJK Sparta Noris Bonifaz",
+    },
+    {
+      date: "Sa. 30.11.25",
+      time: "09:00",
+      homeTeam: "DJK Sparta Noris Bonifaz",
+      guestTeam: "Tuspo Nürnberg Altstadt Kicker (9er)",
+    },
+    {
+      date: "Sa. 07.12.25",
+      time: "09:00",
+      homeTeam: "SpVgg Nürnberg AH/PM",
+      guestTeam: "DJK Sparta Noris Bonifaz",
+    },
+    {
+      date: "Do. 13.03.26",
       time: "19:00",
-      homeTeam: "TSV Kleinschwarzenlohe",
-      guestTeam: "Sparta AH",
+      homeTeam: "ASV Fürth IV Armenia",
+      guestTeam: "DJK Sparta Noris Bonifaz",
     },
     {
-      date: "Fr. 16.05.25",
+      date: "Sa. 22.03.26",
+      time: "09:00",
+      homeTeam: "DJK Sparta Noris Bonifaz",
+      guestTeam: "FC Blau-Schwarz 88",
+    },
+    {
+      date: "Sa. 29.03.26",
+      time: "09:00",
+      homeTeam: "SC Germania Nbg. IIa (9er)",
+      guestTeam: "DJK Sparta Noris Bonifaz",
+    },
+    {
+      date: "Sa. 12.04.26",
+      time: "09:00",
+      homeTeam: "DJK Sparta Noris Bonifaz",
+      guestTeam: "ASV Zirndorf PM Wildner (9er)",
+    },
+    {
+      date: "Do. 17.04.26",
       time: "19:00",
-      homeTeam: "Sparta AH",
-      guestTeam: "TSV Happurg",
+      homeTeam: "Zabo United",
+      guestTeam: "DJK Sparta Noris Bonifaz",
     },
     {
-      date: "Fr. 23.05.25",
-      time: "19:00",
-      homeTeam: "Sparta AH",
-      guestTeam: "TSV Wolkersdorf",
+      date: "Sa. 26.04.26",
+      time: "09:00",
+      homeTeam: "DJK Sparta Noris Bonifaz",
+      guestTeam: "Pegnitzkicker Nbg. Stadtverwaltung (9er)",
     },
     {
-      date: "Mi. 04.06.25",
-      time: "19:30",
-      homeTeam: "SC Worzeldorf",
-      guestTeam: "Sparta AH",
+      date: "Sa. 03.05.26",
+      time: "09:30",
+      homeTeam: "ATV 1873 Frankonia Nbg. Dritte",
+      guestTeam: "DJK Sparta Noris Bonifaz",
     },
     {
-      date: "Mi. 25.06.25",
-      time: "19:00",
-      homeTeam: "Sparta AH",
-      guestTeam: "Boca Seniors",
+      date: "Sa. 10.05.26",
+      time: "09:00",
+      homeTeam: "DJK Sparta Noris Bonifaz",
+      guestTeam: "Kriemhild-Funker (9er)",
     },
     {
-      date: "Fr. 04.07.25",
-      time: "19:00",
-      homeTeam: "Sparta AH",
-      guestTeam: "TSV Langenzenn",
+      date: "Sa. 17.05.26",
+      time: "09:30",
+      homeTeam: "FSV Stadeln 1b Westphal",
+      guestTeam: "DJK Sparta Noris Bonifaz",
     },
     {
-      date: "Mi. 09.07.25",
-      time: "19:00",
-      homeTeam: "SG Schwanstetten",
-      guestTeam: "Sparta AH",
+      date: "Sa. 07.06.26",
+      time: "09:00",
+      homeTeam: "DJK Sparta Noris Bonifaz",
+      guestTeam: "FC Stein II (9er)",
+    },
+    {
+      date: "Sa. 14.06.26",
+      time: "08:45",
+      homeTeam: "Tuspo Nürnberg Altstadt Kicker (9er)",
+      guestTeam: "DJK Sparta Noris Bonifaz",
+    },
+    {
+      date: "Sa. 21.06.26",
+      time: "09:00",
+      homeTeam: "DJK Sparta Noris Bonifaz",
+      guestTeam: "SpVgg Nürnberg AH/PM",
     },
   ];
 
-  return matches.filter((match) => {
-    return convertToDate(match.date, match.time) > new Date();
-  });
+  return matches
+    .filter((match) => {
+      return convertToDate(match.date, match.time) > new Date();
+    })
+    .sort((a, b) => {
+      return (
+        convertToDate(a.date, a.time).getTime() -
+        convertToDate(b.date, b.time).getTime()
+      );
+    });
 }
 
 export function convertToDate(date: string, time: string) {

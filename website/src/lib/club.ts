54c3afb6ca0ebcf1770eb36cd8f20025a0fe05d9
traveling-developer <github@menzel-it.de>
2026-05-25 12:02:46 +0200

    Centralize club information at one place

---
+++ website/src/lib/club.ts
@@ -0,0 +1,31 @@
+export const club = {
+  name: "DJK Sparta Noris Nürnberg e.V.",
+  shortName: "DJK Sparta Noris",
+
+  foundedYear: 1918,
+  fusionYear: 1954,
+
+  memberCount: 500,
+  memberCountDisplay: "~500",
+  departmentCount: 5,
+  teamCount: 18,
+
+  address: {
+    street: "Wacholderweg 60",
+    postalCode: "90441",
+    city: "Nürnberg",
+    district: "Weiherhaus",
+  },
+
+  contact: {
+    phone: "+49 911 480 59 55",
+    phoneTel: "tel:+499114805955",
+    email: "info@djkspartanoris.de",
+    emailMailto: "mailto:info@djkspartanoris.de",
+  },
+
+  coordinates: {
+    latitude: 49.416459843204485,
+    longitude: 11.068967769662304,
+  },
+} as const;
